2be289362431f52fed47760c7790ad229eefdf1f
Asiberus <raphael.beekmann@gmail.com>
2021-09-30 09:23:55 +0200

    chore(front): change dashbopard link to undefined

--- frontend/src/layout/components/TheNavbar.vue
+++ frontend/src/layout/components/TheNavbar.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center pa-3">Tout Doux</h1>
     <v-divider></v-divider>
     <v-list>
-      <v-list-item v-for="(item, i) in menu" :key="i" :to="item.link">
+      <v-list-item v-for="(item, i) in menu" :key="i" :to="item.link" :disabled="!item.link">
         <v-list-item-content>
           <v-list-item-title>{{item.name}}</v-list-item-title>
         </v-list-item-content>
@@ -19,7 +19,7 @@ import {Component, Vue} from "vue-property-decorator";
 export default class TheNavbar extends Vue {
 
   private menu = [
-    {name: 'Dashboard', link: '/'},
+    {name: 'Dashboard'},
     {name: 'Daily', link: {name: 'daily-task-overview'}},
     {name: 'Projects', link: {name: 'project-list'}},
     {name: 'Collections', link: {name: 'collection-list'}},
