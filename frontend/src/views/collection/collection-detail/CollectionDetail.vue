b84c7704aac16f80955d3cd426ef552cc2bccbf7
Asiberus <raphael.beekmann@gmail.com>
2021-10-10 17:23:59 +0200

    feat(front): add collection configuration

--- frontend/src/views/collection/collection-detail/CollectionDetail.vue
+++ frontend/src/views/collection/collection-detail/CollectionDetail.vue
@@ -18,7 +18,7 @@
         <CollectionCompletedTasks :collection="collection"/>
       </v-tab-item>
       <v-tab-item value="configuration" :transition="false">
-        <CollectionConfiguration/>
+        <CollectionConfiguration :collection="collection" :tab="collectionTab"/>
       </v-tab-item>
     </v-tabs-items>
   </v-container>
@@ -68,116 +68,6 @@ export default class CollectionDetail extends Vue {
             }
     );
   }
-
-  // @Watch('createTaskDisplayed')
-  // private onCreateTaskDisplayedChanges(value: boolean): void {
-  //   if (value) {
-  //     this.collection.tasks.unshift({editMode: true} as TaskModel);
-  //   } else {
-  //     // Delete first task if it has no id
-  //     if (!this.collection.tasks[0].id) {
-  //       this.collection.tasks.shift();
-  //     }
-  //   }
-  // }
-
-  // @Watch('editTasksDisplay')
-  // private onEditTasksDisplay(value: boolean): void {
-  //   if (!value) {
-  //     this.disableAllCreatedTasksEditMode();
-  //   }
-  // }
-
-  // private disableAllCreatedTasksEditMode(): void {
-  //   this.collection.tasks.filter((task: TaskModel) => task.id && task.editMode).forEach((task: TaskModel) => {
-  //     task.editMode = false;
-  //   });
-  // }
-
-  // private toggleTaskEditMode(task: TaskModel, value: boolean): void {
-  //   // handle close create task card
-  //   if (!task.id && !value) {
-  //     this.createTaskDisplayed = false;
-  //     return;
-  //   }
-  //
-  //   // Only one card can be in editMode
-  //   this.disableAllCreatedTasksEditMode();
-  //   task.editMode = value;
-  // }
-  //
-  // private updateCollection(collectionForm: Partial<CollectionModel>): void {
-  //   this.collectionEditDialog = false;
-  //   collectionService.updateCollection(this.collection.id, collectionForm).then(
-  //       (response: any) => {
-  //         this.collection.name = response.body.name;
-  //         this.collection.description = response.body.description;
-  //       }, (error: any) => {
-  //         console.error(error);
-  //       }
-  //   )
-  // }
-  //
-  // private deleteCollection(): void {
-  //   this.collectionDeleteDialog = false;
-  //   collectionService.deleteCollection(this.collection.id).then(
-  //       () => {
-  //         this.$router.push({name: 'collection-list'});
-  //       }, (error: any) => {
-  //         console.error(error);
-  //       }
-  //   )
-  // }
-  //
-  // private handleTaskFormSubmit(taskId: number, taskForm: Partial<TaskModel>): void {
-  //   if (taskId) {
-  //     this.updateTask(taskId, taskForm);
-  //   } else {
-  //     this.createTask(taskForm);
-  //   }
-  // }
-  //
-  // private toggleTaskState(taskId: number): void {
-  //   const task = this.collection.tasks.find((task: TaskModel) => task.id === taskId);
-  //   if (!task) {
-  //     return;
-  //   }
-  //
-  //   taskService.updateTaskById(taskId, {completed: !task.completed} as TaskModel).then(
-  //       (response: any) => {
-  //         task.completed = response.body.completed
-  //       }, (error: any) => {
-  //         console.error(error);
-  //       }
-  //   )
-  // }
-  //
-
-  //
-  // private updateTask(taskId: number, taskForm: Partial<TaskModel>): void {
-  //   taskService.updateTaskById(taskId, taskForm).then(
-  //       (response: any) => {
-  //         const task = this.collection.tasks.find((t: TaskModel) => t.id === response.body.id);
-  //         Object.assign(task, response.body, {editMode: false});
-  //       }, (error: any) => {
-  //         console.error(error);
-  //       }
-  //   )
-  // }
-  //
-  // private deleteTask(taskId: number): void {
-  //   taskService.deleteTaskById(taskId).then(
-  //       () => {
-  //         const taskIndex = this.collection.tasks.findIndex((task: TaskModel) => task.id === taskId);
-  //         if (taskIndex !== -1) {
-  //           this.collection.tasks.splice(taskIndex, 1);
-  //         }
-  //       }, (error: any) => {
-  //         console.error(error);
-  //       }
-  //   )
-  // }
-
 }
 </script>
 
