075a232ba9bc10d1f3e722d59f82737d30dae11c
Asiberus <raphael.beekmann@gmail.com>
2021-12-01 00:55:23 +0100

    chore(back): remove list and detail action in section viewset

--- backend/tout_doux/views/section.py
+++ backend/tout_doux/views/section.py
@@ -1,16 +1,17 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 
 from tout_doux.models.section import Section
-from tout_doux.pagination import ExtendedPageNumberPagination
 from tout_doux.serializers.section.section import SectionSerializer
 
 
-class SectionViewSet(viewsets.ModelViewSet):
+class SectionViewSet(mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
-    pagination_class = ExtendedPageNumberPagination
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
