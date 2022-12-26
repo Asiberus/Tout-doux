71ee08ec01d271a66fdf0bf8b56a30ffd8321ccc
Raphael Beekmann <raphael.beekmann@gmail.com>
2022-12-26 20:38:46 +0100

    chore(back): set daily task name when task is deleted

--- backend/tout_doux/models/task.py
+++ backend/tout_doux/models/task.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 from tout_doux.models.collection import Collection
 from tout_doux.models.project import Project
@@ -22,3 +23,11 @@ class Task(models.Model):
     def __str__(self):
         return 'project : {0} - task : {1} - completed : {2}'.format(self.project, self.name,
                                                                      self.completed)
+
+
+@receiver(models.signals.pre_delete, sender=Task)
+def feed_daily_task_name(sender, instance, **kwargs):
+    print(instance.daily_tasks.all(), instance.name)
+    for daily_task in instance.daily_tasks.all():
+        daily_task.name = instance.name
+        daily_task.save()
