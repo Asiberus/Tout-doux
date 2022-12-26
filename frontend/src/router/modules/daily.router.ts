f46e54bb107e5cfabfed18b5979ebe8b5eb8d9c7
Raphael Beekmann <raphael.beekmann@gmail.com>
2022-12-26 20:06:39 +0100

    feat(front): add guard in daily-update route

--- frontend/src/router/modules/daily.router.ts
+++ frontend/src/router/modules/daily.router.ts
@@ -1,5 +1,6 @@
 import DailyOverview from '@/views/daily/daily-overview/DailyOverview.vue'
 import DailyUpdate from '@/views/daily/daily-update/DailyUpdate.vue'
+import moment from 'moment'
 import { RouteConfig } from 'vue-router'
 
 export const dailyRoutes: Array<RouteConfig> = [
@@ -13,5 +14,14 @@ export const dailyRoutes: Array<RouteConfig> = [
         name: 'daily-update',
         component: DailyUpdate,
         props: ({ params }) => ({ date: params.date, step: params.step }),
+        beforeEnter: (to, from, next) => {
+            const { date, step } = to.params
+            if (moment().isSame(date, 'day')) next()
+            else
+                next({
+                    name: 'daily-update',
+                    params: { date: moment().format('YYYY-MM-DD'), step },
+                })
+        },
     },
 ]
