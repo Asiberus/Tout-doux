5c9a8b5df85f9e3970685e03b34be39f837fa2e7
Raphael Beekmann <raphael.beekmann@gmail.com>
2023-11-23 14:57:40 +0100

    chore(back): delete txt mails

--- backend/tout_doux/services/email.py
+++ backend/tout_doux/services/email.py
@@ -29,15 +29,13 @@ class EmailService:
         link = f'{settings.SERVER_URL}activate?uidb64={uidb64}&token={token}'
         context = {'user': user, 'link': link}
 
-        html_template = get_template('email/html/user-creation.html')
-        text_template = get_template('email/text/user-creation.txt')
+        html_template = get_template('email/user-creation.html')
         html_body = html_template.render(context)
-        text_body = text_template.render(context)
 
         subject = 'Tout Doux - Création de compte'
         to = (user.email,)
 
-        EmailService._send_mail_async(subject=subject, to=to, body=text_body, html_body=html_body)
+        EmailService._send_mail_async(subject=subject, to=to, html_body=html_body)
 
     @staticmethod
     def send_reset_password_email(user):
@@ -46,22 +44,20 @@ class EmailService:
         link = f'{settings.SERVER_URL}password-reset?uidb64={uidb64}&token={token}'
         context = {'user': user, 'link': link}
 
-        html_template = get_template('email/html/password-reset.html')
-        text_template = get_template('email/text/password-reset.txt')
+        html_template = get_template('email/password-reset.html')
         html_body = html_template.render(context)
-        text_body = text_template.render(context)
 
         subject = 'Tout Doux - Réinitialisation de mot de passe'
         to = (user.email,)
 
-        EmailService._send_mail_async(subject=subject, to=to, body=text_body, html_body=html_body)
+        EmailService._send_mail_async(subject=subject, to=to, html_body=html_body)
 
     @staticmethod
     def send_change_email_request_email(user, new_email, token):
         link = f'{settings.SERVER_URL}confirm-email?token={token}'
         context = {'user': user, 'link': link}
 
-        html_template = get_template('email/html/change-email.html')
+        html_template = get_template('email/change-email.html')
         html_body = html_template.render(context)
 
         subject = 'Tout Doux - Changement d\'adresse e-mail'
@@ -73,7 +69,7 @@ class EmailService:
     def send_email_changed_email(user, previous_email):
         context = {'user': user}
 
-        html_template = get_template('email/html/email-has-changed.html')
+        html_template = get_template('email/email-has-changed.html')
         html_body = html_template.render(context)
 
         subject = 'Tout Doux - Un changement d\'adresse à été effectué'
