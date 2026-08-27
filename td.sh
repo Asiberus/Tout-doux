1dac32e8d0183216d249c19973de6d0c3201ef80
Raphael Beekmann <raphael.beekmann@gmail.com>
2026-08-27 23:25:53 +0200

    chore: change version to v0.6.0

--- td.sh
+++ td.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-vers="0.5.0" # Must match package.json version number
+vers="0.6.0" # Must match package.json version number
 basedir=$(dirname "${0}")
 unset backendsecretkey
 unset serverhost
